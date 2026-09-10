2701e9b6feeec1c2934ef275265c7c753e149672
William Silvano Angga <williams.ang28@gmail.com>
2026-09-10 14:12:57 +0800

    first commit

---
+++ ChessUp/ChessUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessUpApp.swift
+//  ChessUp
+//
+//  Created by William Silvano Angga on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ChessUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
